007e5188fa3bf1cc24849c12289e9fd8abe27542
wanyuasam <samuelwanyua@gmail.com>
2024-08-21 09:08:03 +0300

    Add modal

--- starter/src/App.js
+++ starter/src/App.js
@@ -3,9 +3,11 @@ import CardContainer from "./components/CardContainer";
 import Nav from "./components/Nav";
 import { calculateTotals } from "./features/cart/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
+import Modal from "./components/Modal";
 
 function App() {
   const {cartItems} = useSelector(store => store.cart)
+  const {isOpen} = useSelector(state => state.modal)
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,6 +17,7 @@ function App() {
 
   return( 
   <main>
+    <Modal/>
     <Nav/>
     <CardContainer/>
   </main>
---
+++ starter/src/components/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Modal() {
+  return (
+    <aside className="modal-container">
+        <div className='modal'>
+          <h4>Remove all items from your shopping cart?</h4>
+          <div className='btn-container'>
+            <button type='button' className='btn confirm-btn'>Confirm</button>
+            <button type='button' className='btn clear-btn'>Cancel</button>
+          </div>
+        </div>
+    </aside>
+  )
+}
+
+export default Modal
